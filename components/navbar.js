2d6fc2992e24f52b0df7ccb3eea82145c5e2f87f
Juan Dematei <hola@juandematei.com>
2023-02-10 11:15:53 -0300

    url

--- components/navbar.js
+++ components/navbar.js
@@ -8,37 +8,37 @@ class Navbar extends HTMLElement {
           <nav id="desktop">
           <div class="container">
             <div class="logo">
-            <a href="/index.html"><img src="/img/logo-spacial-dynamics.png" alt="Spacial Dynamics Logo" /></a>
+            <a href="index.html"><img src="img/logo-spacial-dynamics.png" alt="Spacial Dynamics Logo" /></a>
             </div>
             <ul class="navbar">
               <li class="navbar-link">
-                <a href="/index.html">Inicio</a>
+                <a href="index.html">Inicio</a>
               </li>
               <li class="navbar-link">
-                <a href="/pages/sobre-nosotros.html">Sobre nosotros</a>
+                <a href="pages/sobre-nosotros.html">Sobre nosotros</a>
               </li>
               <li class="navbar-link">
                 <a href="javascript:void(0)" class="dropdown-toggle">Sobre la formación<i class="fas fa-chevron-down"></i></a>
                 <ul class="dropdown-menu">
                   <li>
-                    <a href="/pages/sobre-la-formacion/objetivos-del-programa.html">Objetivos del Programa</a>
+                    <a href="pages/sobre-la-formacion/objetivos-del-programa.html">Objetivos del Programa</a>
                   </li>
                   <li>
-                    <a href="/pages/sobre-la-formacion/ambitos-de-aplicacion.html">Ámbitos de aplicación</a>
+                    <a href="pages/sobre-la-formacion/ambitos-de-aplicacion.html">Ámbitos de aplicación</a>
                   </li>
                   <li>
-                    <a href="/pages/sobre-la-formacion/metodos-pedagogicos.html">Métodos pedagógicos</a>
+                    <a href="pages/sobre-la-formacion/metodos-pedagogicos.html">Métodos pedagógicos</a>
                   </li>
                   <li>
-                    <a href="/pages/sobre-la-formacion/niveles-de-formacion.html">Niveles de formación</a>
+                    <a href="pages/sobre-la-formacion/niveles-de-formacion.html">Niveles de formación</a>
                   </li>
                   <li>
-                    <a href="/pages/sobre-la-formacion/lineamientos-pedagogicos.html">Lineamietos pedagógicos</a>
+                    <a href="pages/sobre-la-formacion/lineamientos-pedagogicos.html">Lineamietos pedagógicos</a>
                   </li>
                 </ul>
               </li>
               <li class="navbar-link">
-                <a href="/pages/sinergias.html">Sinergias</a>
+                <a href="pages/sinergias.html">Sinergias</a>
               </li>
             </ul>
           </div>
