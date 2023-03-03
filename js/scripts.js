b7b3987e366e8c20f698bbafa881c91bc6b3a95f
Juan Dematei <hola@juandematei.com>
2023-03-02 21:51:15 -0300

    navbar

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,12 @@ const mainMenuElement = document.getElementById("main-menu");
 toggleMenuElement.addEventListener("click", () => {
   mainMenuElement.classList.toggle("main-menu__show");
 });
+
+const toggleDropdownElement = document.getElementById("toggle-dropdown");
+const toggleDropdownIcon = document.getElementById("toggle-dropdown__icon");
+const dropdownMenuElement = document.getElementById("dropdown-menu");
+
+toggleDropdownElement.addEventListener("click", () => {
+  dropdownMenuElement.classList.toggle("dropdown-menu__show");
+  toggleDropdownIcon.classList.toggle("rotate");
+});
