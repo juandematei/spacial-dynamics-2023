7ecb6590568f5d34d901d998d752533fe9846d7b
Juan Dematei <hola@juandematei.com>
2023-03-05 17:49:49 -0300

    detalles finales

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
-const toggleMenuElement = document.getElementById("toggle-menu");
-const mainMenuElement = document.getElementById("main-menu");
+const toggleMenuElement = document.getElementById('toggle-menu');
+const mainMenuElement = document.getElementById('main-menu');
 
-toggleMenuElement.addEventListener("click", () => {
-  mainMenuElement.classList.toggle("main-menu__show");
+toggleMenuElement.addEventListener('click', () => {
+  mainMenuElement.classList.toggle('main-menu__show');
 });
 
-const toggleDropdownElement = document.getElementById("toggle-dropdown");
-const toggleDropdownIcon = document.getElementById("toggle-dropdown__icon");
-const dropdownMenuElement = document.getElementById("dropdown-menu");
+const toggleDropdownElement = document.getElementById('toggle-dropdown');
+const toggleDropdownIcon = document.getElementById('toggle-dropdown__icon');
+const dropdownMenuElement = document.getElementById('dropdown-menu');
 
-toggleDropdownElement.addEventListener("click", () => {
-  dropdownMenuElement.classList.toggle("dropdown-menu__show");
-  toggleDropdownIcon.classList.toggle("rotate");
+toggleDropdownElement.addEventListener('click', () => {
+  dropdownMenuElement.classList.toggle('dropdown-menu__show');
+  toggleDropdownIcon.classList.toggle('rotate');
 });
